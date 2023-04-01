708809d51ab6603ce2f4a4e900618f032036b4d2
Petr Ohlidal <petr@ohlidal.cz>
2023-04-01 13:59:55 +0200

    codechange: Renamed `EngineSim` funcs back to original :boomerang:
    This touches many files but it's just find&replace of the renamed functions in EngineSim.h
    This is a preparation for AngelScript bindings which must all be `camelCase()`.

--- source/main/physics/ActorSpawner.cpp
+++ source/main/physics/ActorSpawner.cpp
@@ -423,7 +423,7 @@ void ActorSpawner::FinalizeRig()
         }
 
         //Gearbox
-        m_actor->ar_engine->SetAutoMode(App::sim_gearbox_mode->getEnum<SimGearboxMode>());
+        m_actor->ar_engine->setAutoMode(App::sim_gearbox_mode->getEnum<SimGearboxMode>());
     }
     
     // Sanitize trigger_cmdshort and trigger_cmdlong
@@ -5413,7 +5413,7 @@ void ActorSpawner::ProcessEngine(RigDef::Engine & def)
     );
 
     /* Apply game configuration */
-    m_actor->ar_engine->SetAutoMode(App::sim_gearbox_mode->getEnum<SimGearboxMode>());
+    m_actor->ar_engine->setAutoMode(App::sim_gearbox_mode->getEnum<SimGearboxMode>());
 };
 
 void ActorSpawner::ProcessHelp(RigDef::Help & def)
@@ -6289,7 +6289,7 @@ void ActorSpawner::SetupDefaultSoundSources(ActorPtr const& vehicle)
         }
         if (vehicle->ar_engine->m_engine_type == 'c')
             AddSoundSourceInstance(vehicle, "tracks/default_car", ar_exhaust_pos_node);
-        if (vehicle->ar_engine->HasTurbo())
+        if (vehicle->ar_engine->hasTurbo())
         {
             if (vehicle->ar_engine->m_turbo_inertia_factor >= 3)
                 AddSoundSourceInstance(vehicle, "tracks/default_turbo_big", ar_exhaust_pos_node);
