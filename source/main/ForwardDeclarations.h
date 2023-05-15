2e44c311b59cda54169a259305310050575725d3
Petr Ohlidal <petr@ohlidal.cz>
2023-05-15 02:01:04 +0200

    Codechange: `node_t*` -> `NodeNum_t` in `ropable_t`
    Prepwork for making ar_nodes a std::vector - better diagnostics and flexibility

--- source/main/ForwardDeclarations.h
+++ source/main/ForwardDeclarations.h
@@ -99,6 +99,9 @@ namespace RoR
     typedef int RepoFileInstallRequestID_t; //!< Unique sequentially generated ID of a repository item installation request; use `GUI::RepositorySelector::GetNextInstallRequestId()`.
     static const RepoFileInstallRequestID_t REPOFILEINSTALLREQUESTID_INVALID = -1; //!< Invalid ID for repository item installation request.
 
+    typedef int RopableID_t; //!< Index to `Actor::ar_ropables`, use RoR::ROPABLEID_INVALID as empty value.
+    static const RopableID_t ROPABLEID_INVALID = -1;
+
     class  Actor;
     class  ActorManager;
     class  ActorSpawner;
